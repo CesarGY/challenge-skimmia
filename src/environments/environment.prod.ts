97c1d8466a4c78c32006368c51e6f44d6b0ec735
Cesar <cesar.glez235@gmail.com>
2021-02-01 16:17:15 -0600

    Env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+    production: true,
+    firebaseConfig: {
+        apiKey: 'AIzaSyAaQe69UsPudVLuTvN8WiVMUQBrcy4jG7k',
+        authDomain: 'challenge-skimmia.firebaseapp.com',
+        projectId: 'challenge-skimmia',
+        storageBucket: 'challenge-skimmia.appspot.com',
+        messagingSenderId: '310145651215',
+
+    }
 };
